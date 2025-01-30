97f69f25d8d5c5741d827af672f46aefbfa88333
Quant Fund at Cornell <81406511+CornellQuantFund@users.noreply.github.com>
2025-01-30 15:27:38 -0500

    Update config.js

--- config/config.js
+++ config/config.js
@@ -57,11 +57,8 @@ export const work = {
     {
       title: "Aryaan Jena",
       description: "Junior studying Math.",
-      icons: [
-          {
-            icon: linkedIn.src,
-            link: "https://www.linkedin.com/in/aryaan-jena-332a00251/",
-          }],
+      icon: linkedIn.src,
+      link: "https://www.linkedin.com/in/aryaan-jena-332a00251/",
     },
     {
       title: "Olu Ogundare",
